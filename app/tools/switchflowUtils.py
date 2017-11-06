8b5350625ed9a6913d199f684cd8294b2455cfd4
Kylin <unknow_code@163.com>
2017-11-06 11:27:25 +0800

    change requests to python-consul

--- app/tools/switchflowUtils.py
+++ app/tools/switchflowUtils.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests
-import base64
+
 from flask import current_app
 from app.tools.jsonUtils import response_json
-from requests.exceptions import Timeout
-from urlparse import urljoin
+from app.tools.connectpoolUtils import create_consul_connection
 
 
 def registed_service(scope="", service=""):
@@ -14,39 +12,33 @@ def registed_service(scope="", service=""):
     :param service: 当scope不为all时 service来指定获取指定服务的backend信息
     :return:
     """
-    ret = {}
     try:
-        r = requests.get(urljoin(current_app.config['CONSUL_BASE_URL'], "?recurse"), timeout=12)
-    except Timeout, e:
-        current_app.logger.error("query upstream backend timeout from consul http api, message:{0}".format(e))
-        return response_json(503, "time out", "")
-    else:
-        all_service = r.json()
-        if all_service:
-            if scope == "all":
-                for s in all_service:
-                    backend_attribute = s['Value']
-                    per_upstream = s['Key'].split('/')
-                    service_name = per_upstream[1]
-                    ip = per_upstream[2].split(':')[0]
-                    port = per_upstream[2].split(':')[1]
-                    ret.setdefault(service_name, []).append({"ip": ip, "port": port,
-                                                             "attribute": eval(base64.b64decode(backend_attribute))
-                                                             })
-            else:
-                for s in all_service:
-                    per_upstream = s['Key'].split('/')
-                    service_name = per_upstream[1]
-                    if service_name == service:
-                        backend_attribute = s['Value']
-                        ip = per_upstream[2].split(':')[0]
-                        port = per_upstream[2].split(':')[1]
-                        ret.setdefault(service_name, []).append({"ip": ip, "port": port,
-                                                                 "attribute": eval(base64.b64decode(backend_attribute))}
-                                                                )
-            return ret
+        c = create_consul_connection()
+        res = c.kv.get('', keys=True, recurse=True)[1]
+    except Exception, e:
+        current_app.logger.error("query upstream backend timeout from consul , message:{0}".format(e))
+        return response_json(503, "consul service error", "")
+    ret = {}
+    if res:
+        if scope == 'all':
+            for ups in res:
+                ip = ups.split('/')[2].split(':')[0]
+                port = ups.split('/')[2].split(':')[1]
+                attr = c.kv.get(key=ups, recurse=True)[1][0]['Value']
+                ret.setdefault(ups.split('/')[1], []).append({"ip": ip, "port": port,
+                                                              "attribute": eval(attr)})
         else:
-            return {}
+            for ups in res:
+                svc = ups.split('/')[1]
+                if svc == service:
+                    ip = ups.split('/')[2].split(':')[0]
+                    port = ups.split('/')[2].split(':')[1]
+                    attr = c.kv.get(key=ups, recurse=True)[1][0]['Value']
+                    ret.setdefault(ups.split('/')[1], []).append({"ip": ip, "port": port,
+                                                                  "attribute": eval(attr)})
+    return ret
+
+
 
 
 

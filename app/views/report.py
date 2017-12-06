e47ecd820d70fad02321c1b55f9172d6a2833f7b
Kylin <unknow_code@163.com>
2017-12-06 13:52:02 +0800

    统一接口 优化接口调用次数

--- app/views/report.py
+++ app/views/report.py
@@ -10,24 +10,6 @@ report = Blueprint("report", __name__)
 
 
 @report.route('/overview')
-def overview():
-    """
-    获取上周上线次数 天数为纬度
-    :return: like: [10,34,56,23,10]
-    """
-    last_deploy_flow_count = []
-    last_week_date_res = last_week_date()
-    for index, value in enumerate(last_week_date_res):
-        if index != len(last_week_date_res)-1:
-            start_date = datetime.datetime.strptime(value, "%Y-%m-%d")
-            end_date = datetime.datetime.strptime(last_week_date_res[index+1], "%Y-%m-%d")
-            w_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                              (Workflow.type == 1)).count()
-            last_deploy_flow_count.append(w_count)
-    return response_json(200, '', last_deploy_flow_count)
-
-
-@report.route('/overview/table')
 def overview_table():
     """
     获取上周上线详情 天数为纬度
@@ -62,37 +44,3 @@ def overview_table():
                                      'except': except_count,
                                      'open': open_count})
     return response_json(200, '', last_deploy_flow)
-
-
-@report.route('/overview/pie')
-def overview_pie():
-    """
-    获取上周上线详情 天数为纬度
-    :return:
-    """
-    last_week_date_res = last_week_date()
-    start_date = datetime.datetime.strptime(last_week_date_res[0], "%Y-%m-%d")
-    end_date = datetime.datetime.strptime(last_week_date_res[-1], "%Y-%m-%d")
-    total_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                          (Workflow.type == 1)).count()
-    # open flow
-    open_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                         (Workflow.type == 1) & (Workflow.status == 3)).count()
-
-    # success flow
-    success_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                            (Workflow.type == 1) & (Workflow.status == 4)).count()
-    # abort flow
-    abort_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                          (Workflow.type == 1) & (Workflow.status == 5)).count()
-
-    # except flow
-    except_count = Workflow.select().where((Workflow.create_time.between(start_date, end_date)) &
-                                           (Workflow.type == 1) & (Workflow.status == 6)).count()
-    ret = {'date': '',
-           'total': total_count,
-           'success': success_count,
-           'abort': abort_count,
-           'except': except_count,
-           'open': open_count}
-    return response_json(200, '', ret)
